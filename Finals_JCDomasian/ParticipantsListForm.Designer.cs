c7d3f6e3dd84b82e2d6c6db0843ddbfa82ee8386
JC <jhoncarlokristina@gmail.com>
2025-12-12 08:57:18 +0800

    Initial commit - Milo Marathon Registration System

--- Finals_JCDomasian/ParticipantsListForm.Designer.cs
+++ Finals_JCDomasian/ParticipantsListForm.Designer.cs
@@ -28,13 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSearchName = new System.Windows.Forms.Label();
+            this.txtSearchName = new System.Windows.Forms.TextBox();
+            this.lblMarathonType = new System.Windows.Forms.Label();
+            this.cmbMarathonType = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.dgvParticipants = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParticipants)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(350, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(424, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Milo Marathon Registration System";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddNew);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.btnRefresh);
+            this.groupBox1.Controls.Add(this.cmbMarathonType);
+            this.groupBox1.Controls.Add(this.lblMarathonType);
+            this.groupBox1.Controls.Add(this.txtSearchName);
+            this.groupBox1.Controls.Add(this.lblSearchName);
+            this.groupBox1.Location = new System.Drawing.Point(20, 60);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(960, 124);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search & Filter";
+            // 
+            // lblSearchName
+            // 
+            this.lblSearchName.AutoSize = true;
+            this.lblSearchName.Location = new System.Drawing.Point(20, 30);
+            this.lblSearchName.Name = "lblSearchName";
+            this.lblSearchName.Size = new System.Drawing.Size(75, 13);
+            this.lblSearchName.TabIndex = 0;
+            this.lblSearchName.Text = "Search Name:";
+            // 
+            // txtSearchName
+            // 
+            this.txtSearchName.Location = new System.Drawing.Point(120, 27);
+            this.txtSearchName.Name = "txtSearchName";
+            this.txtSearchName.Size = new System.Drawing.Size(200, 20);
+            this.txtSearchName.TabIndex = 1;
+            // 
+            // lblMarathonType
+            // 
+            this.lblMarathonType.AutoSize = true;
+            this.lblMarathonType.Location = new System.Drawing.Point(350, 30);
+            this.lblMarathonType.Name = "lblMarathonType";
+            this.lblMarathonType.Size = new System.Drawing.Size(82, 13);
+            this.lblMarathonType.TabIndex = 2;
+            this.lblMarathonType.Text = "Marathon Type:";
+            // 
+            // cmbMarathonType
+            // 
+            this.cmbMarathonType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMarathonType.FormattingEnabled = true;
+            this.cmbMarathonType.Location = new System.Drawing.Point(470, 27);
+            this.cmbMarathonType.Name = "cmbMarathonType";
+            this.cmbMarathonType.Size = new System.Drawing.Size(150, 21);
+            this.cmbMarathonType.TabIndex = 3;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(760, 25);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(90, 30);
+            this.btnRefresh.TabIndex = 4;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(650, 25);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(90, 30);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click_1);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(23, 88);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(150, 30);
+            this.btnAddNew.TabIndex = 6;
+            this.btnAddNew.Text = "Add New Participant";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // dgvParticipants
+            // 
+            this.dgvParticipants.AllowUserToAddRows = false;
+            this.dgvParticipants.AllowUserToDeleteRows = false;
+            this.dgvParticipants.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvParticipants.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvParticipants.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvParticipants.Location = new System.Drawing.Point(20, 190);
+            this.dgvParticipants.Name = "dgvParticipants";
+            this.dgvParticipants.ReadOnly = true;
+            this.dgvParticipants.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvParticipants.Size = new System.Drawing.Size(960, 350);
+            this.dgvParticipants.TabIndex = 2;
+            // 
+            // ParticipantsListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(997, 629);
+            this.Controls.Add(this.dgvParticipants);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "ParticipantsListForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Milo Marathon - Participants List";
+            this.Load += new System.EventHandler(this.ParticipantsListForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvParticipants)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblSearchName;
+        private System.Windows.Forms.TextBox txtSearchName;
+        private System.Windows.Forms.Label lblMarathonType;
+        private System.Windows.Forms.ComboBox cmbMarathonType;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.DataGridView dgvParticipants;
     }
 }
 
--- Finals_JCDomasian/ParticipantsListForm.cs
+++ Finals_JCDomasian/ParticipantsListForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Finals_JCDomasian.DataAccess;
+using FinalsTP_YourLastName.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,173 @@ namespace Finals_JCDomasian
 {
     public partial class ParticipantsListForm : Form
     {
+        private DbHelper dbHelper;
+
         public ParticipantsListForm()
         {
             InitializeComponent();
+            dbHelper = new DbHelper();
+        }
+
+        // Form Load - runs when form opens
+        private void ParticipantsListForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                // Load marathon types into ComboBox
+                LoadMarathonTypes();
+
+                // Load all participants into DataGridView
+                LoadParticipants();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Load all participants
+        private void LoadParticipants()
+        {
+            try
+            {
+                DataTable dt = dbHelper.GetAllParticipants();
+                dgvParticipants.DataSource = dt;
+
+                // Format columns
+                FormatDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading participants: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Load marathon types into ComboBox
+        private void LoadMarathonTypes()
+        {
+            try
+            {
+                cmbMarathonType.Items.Clear();
+                cmbMarathonType.Items.Add("All");
+                cmbMarathonType.Items.Add("5km");
+                cmbMarathonType.Items.Add("10km");
+                cmbMarathonType.Items.Add("25km");
+                cmbMarathonType.SelectedIndex = 0; // Select "All" by default
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading marathon types: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Format DataGridView columns
+        private void FormatDataGridView()
+        {
+            if (dgvParticipants.Columns.Count > 0)
+            {
+                // Hide ParticipantID column
+                if (dgvParticipants.Columns.Contains("ParticipantID"))
+                    dgvParticipants.Columns["ParticipantID"].Visible = false;
+
+                // Set column headers
+                if (dgvParticipants.Columns.Contains("FullName"))
+                    dgvParticipants.Columns["FullName"].HeaderText = "Name";
+
+                if (dgvParticipants.Columns.Contains("BirthDate"))
+                {
+                    dgvParticipants.Columns["BirthDate"].HeaderText = "Birthday";
+                    dgvParticipants.Columns["BirthDate"].DefaultCellStyle.Format = "MM/dd/yyyy";
+                }
+
+                if (dgvParticipants.Columns.Contains("Phone"))
+                    dgvParticipants.Columns["Phone"].HeaderText = "Phone";
+
+                if (dgvParticipants.Columns.Contains("Address"))
+                    dgvParticipants.Columns["Address"].HeaderText = "Address";
+
+                if (dgvParticipants.Columns.Contains("BibNumber"))
+                    dgvParticipants.Columns["BibNumber"].HeaderText = "Bib #";
+
+                if (dgvParticipants.Columns.Contains("MarathonType"))
+                    dgvParticipants.Columns["MarathonType"].HeaderText = "Marathon Type";
+
+                if (dgvParticipants.Columns.Contains("Price"))
+                {
+                    dgvParticipants.Columns["Price"].HeaderText = "Price";
+                    dgvParticipants.Columns["Price"].DefaultCellStyle.Format = "₱#,##0.00";
+                }
+
+                if (dgvParticipants.Columns.Contains("PaymentStatus"))
+                    dgvParticipants.Columns["PaymentStatus"].HeaderText = "Status";
+            }
+        }
+
+        // Search button click
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nameFilter = txtSearchName.Text.Trim();
+                string marathonType = cmbMarathonType.SelectedItem?.ToString() ?? "All";
+
+                DataTable dt = dbHelper.SearchParticipants(nameFilter, marathonType);
+                dgvParticipants.DataSource = dt;
+                FormatDataGridView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error searching: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Refresh button click
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Clear filters
+                txtSearchName.Clear();
+                cmbMarathonType.SelectedIndex = 0;
+
+                // Reload all data
+                LoadParticipants();
+
+                MessageBox.Show("Data refreshed successfully!", "Success",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error refreshing: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Add New Participant button click
+        private void btnAddNew_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RegistrationForm regForm = new RegistrationForm();
+                regForm.ShowDialog(); // Open as modal dialog
+
+                // Refresh data after registration form closes
+                LoadParticipants();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening registration form: " + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSearch_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
